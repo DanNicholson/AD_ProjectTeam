ede61000c32df115b91418193c5a27948e63c7eb
Daniel Nicholson <32296527+DanNicholson@users.noreply.github.com>
2017-11-09 20:12:35 +0000

    Added temp player object, started writing script
    I have created a quad and added both a rigidbody and a C# script to test movement

---
+++ ProjectSpaceShooterPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(playerSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(-playerSpeed, 0, 0);
+        }
+
+    }
+}
