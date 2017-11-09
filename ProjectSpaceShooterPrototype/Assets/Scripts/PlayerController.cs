30ec3eb097edefdbe1cdbd8e547d2e531a8cd67b
Daniel Nicholson <32296527+DanNicholson@users.noreply.github.com>
2017-11-09 21:43:49 +0000

    Movement System updated, Shooting System added
    >I've updated the movement system so it doesn't just slide around. It's still controlled with the arrow keys.
    >Added a basic shooting system, activated with space bar

--- ProjectSpaceShooterPrototype/Assets/Scripts/PlayerController.cs
+++ ProjectSpaceShooterPrototype/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //PLAYER MOVEMENT VARIABLE
     public float playerSpeed;
+
+    //PROJECTILE VARIABLE
+    public GameObject Bullet;
+    public float rateOfFire;
+    public float bulletSpeed;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+    void Shooting()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0, 0.4f, 0);
+        GameObject attack = Instantiate(Bullet, startPosition, Quaternion.identity) as GameObject;
+        attack.GetComponent<Rigidbody2D>().velocity = new Vector3(0, bulletSpeed, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //MOVEMENT CONTROLS
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(playerSpeed, 0, 0);
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * playerSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(-playerSpeed, 0, 0);
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * playerSpeed * Time.deltaTime;
         }
 
+        //SHOOTING
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Shooting", 0.0001f, rateOfFire);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Shooting");
+        }
     }
 }
